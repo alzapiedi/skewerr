7515e29229079b61e171da20553569f07f85f0bb
Nick Alzapiedi <nick@Nicks-MBP-2.fios-router.home>
2018-10-10 03:18:23 -0400

    canvas in popup

--- popup.js
+++ popup.js
@@ -1,11 +1,51 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
+const HEIGHT = 300;
+const WIDTH = 480;
+const GRID_COLOR = '#eee';
 
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+const canvas = document.getElementById('canvas');
+canvas.height = HEIGHT;
+canvas.width = WIDTH;
+const ctx = canvas.getContext('2d');
+
+function drawGrid() {
+  ctx.lineWidth = 1;
+  const interval = WIDTH / 10;
+  for (let i = 1; i * interval < WIDTH / 2; i++) {
+    const x1 = (WIDTH / 2) + (interval * i);
+    const x2 = (WIDTH / 2) - (interval * i);
+    drawLine(x1, 0, x1, HEIGHT);
+    drawLine(x2, 0, x2, HEIGHT);
+  }
+  for (let i = 1; i * interval < HEIGHT; i++) {
+    const y = HEIGHT - (i*interval)
+    drawLine(0, y, WIDTH, y);
+  }
+}
+
+function drawLine(x1,y1,x2,y2,color = GRID_COLOR, weight = 1) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle=color;
+  ctx.lineWidth = weight;
+  ctx.stroke();
+}
+
+function drawAxes() {
+  drawLine(0, HEIGHT - 1, WIDTH, HEIGHT - 1, '#ccc', 2);
+  drawLine(WIDTH / 2, 0, WIDTH / 2, HEIGHT, '#ccc', 2);
+}
+
+function drawGradient() {
+  const gradient = ctx.createLinearGradient(0,0,WIDTH,0);
+  gradient.addColorStop(0, '#ddddff');
+  gradient.addColorStop(1, '#ffdddd');
+
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0,0,WIDTH,HEIGHT);
+}
+
+drawGradient();
+drawGrid();
+drawAxes();
